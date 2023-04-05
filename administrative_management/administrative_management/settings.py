ea61574c4455b3d4f835f721357641f08008d757
ValenOrduna <valenordu@hotmail.com>
2023-04-04 22:03:36 -0300

    Project Completed

--- administrative_management/administrative_management/settings.py
+++ administrative_management/administrative_management/settings.py
@@ -6,7 +6,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-sxtthwriscz&(a$jef8x++d=cchelyv6w9auaw)dxjt7u!xz=$'
